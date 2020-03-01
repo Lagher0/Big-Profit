4f0807bdd176ab1f7e87e76023023f2e27e6da4a
Sophia <TheGiraffe@users.noreply.github.com>
2020-03-01 07:15:55 +0000

    frontend update

--- web_project/views.py
+++ web_project/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from hunt import hunt
 
 def index(request):
     return render(request, 'index.html')
 
 def startadventure(request):
     if request.method == 'POST':
-        score = 3
-        time_constraint = request.POST['time_option']
+        time_constraint = float(request.POST['time_option'])
         place_category = request.POST.getlist('category')
-        radius = request.POST['distance_option']
-        return render(request,'startadventure.html',{"time_option":time_constraint, "category":place_category, "distance_option":radius,"score":score})
+        radius = float(request.POST['distance_option'])
+        dahunt = hunt(time_constraint,radius)
+        return render(request,'startadventure.html',{"time_option":time_constraint, "category":place_category, "distance_option":radius,"dahunt":dahunt})
